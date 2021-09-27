0994fc01af848fe34ed05af2105e6d93a3d92078
MrCasco <ernesto.casco.velazquez@hotmail.com>
2021-09-27 12:32:01 -0500

    Funcion comenzar(): Done

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('¿Bienvenid@ desarrollador/a quieres tomar nuestro challenge de programación?');
+console.log('Teclea comenzar() para empezar');
+
+function comenzar() {
+  console.log('Dado un número entero, tienes que determinar si es \'PAR\' o \'IMPAR\'');
+  console.log('Ejemplo:');
+  console.log('Input: 34, Output: PAR');
+  console.log('Input: 5, Output: IMPAR');
+  console.log('Input: -1, Output: IMPAR');
+  console.log('Elabora tu función de esta manera:');
+  console.log('function parImpar(x){');
+  console.log('    ...');
+  console.log('};');
+  console.log('Cuando hayas temrinado, teclea probarParImpar() para probar tu código!');
+}
